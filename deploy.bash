b7211ce929f48a896996e5c7eb4baeb9e989898a
zerokqx <swiftkey382@gmail.com>
2025-12-24 15:09:15 +0300

    feat(automatization)

--- deploy.bash
+++ deploy.bash
@@ -1,7 +1,2 @@
-
-name_app="nova-ai"
-docker stop $name_app
-docker rm $name_app
-docker rm $name_app
-docker build -t $name_app .
-docker run  --name $name_app -p 80:80 $name_app
+bun i
+bun nova:build:all
